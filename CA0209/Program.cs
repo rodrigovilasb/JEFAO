3fbfc0e8f5f888e1ba5619da24b9c9acd894fad5
Jeferson Ferreira <jeferson.oferreira@etec.sp.gov.br>
2025-09-02 17:11:34 -0300

    projeto inicial

---
+++ CA0209/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA0209
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
